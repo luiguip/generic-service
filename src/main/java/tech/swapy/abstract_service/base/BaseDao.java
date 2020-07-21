52e46251c42abb7a0c27281ee5d9a27dba8868c2
luiguip <luisguilhermepdea@gmail.com>
2020-07-21 15:51:39 -0300

    [REF] Base repository refatoration

--- src/main/java/tech/swapy/abstract_service/base/BaseDao.java
+++
@@ -1,9 +0,0 @@
-package tech.swapy.abstract_service.base;
-
-import java.io.Serializable;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface BaseDao<T extends BaseModel> extends CrudRepository<T, Serializable>{
-
-}
