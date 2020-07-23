981fdd59fda0455447c8b59b8db4b8d7ba9db377
luiguip <luisguilhermepdea@gmail.com>
2020-07-23 14:06:09 -0300

    [ADD] test of reflexivity of BaseModel equals

--- src/test/java/tech/swapy/abstract_service/base/BaseModelTests.java
+++ src/test/java/tech/swapy/abstract_service/base/BaseModelTests.java
@@ -1,5 +1,25 @@
 package tech.swapy.abstract_service.base;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
 public class BaseModelTests {
 
+	@Test
+	public void equals() {
+		BaseModel baseModelX = this.createBaseModel();
+
+		reflexivity(baseModelX);
+	}
+
+	public BaseModel createBaseModel() {
+		BaseModel baseModel = new BaseModel();
+		baseModel.setId(1L);
+		return baseModel;
+	}
+
+	public void reflexivity(BaseModel baseModel) {
+		assertTrue(baseModel.equals(baseModel));
+	}
 }
