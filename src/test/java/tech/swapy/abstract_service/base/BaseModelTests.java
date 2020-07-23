8468ec2f3e10f29999593c8be11c0fc3efc7b0b4
luiguip <luisguilhermepdea@gmail.com>
2020-07-23 14:54:02 -0300

    [ADD] simetricity test for BaseModel equals

--- src/test/java/tech/swapy/abstract_service/base/BaseModelTests.java
+++ src/test/java/tech/swapy/abstract_service/base/BaseModelTests.java
@@ -9,8 +9,10 @@ public class BaseModelTests {
 	@Test
 	public void equals() {
 		BaseModel baseModelX = this.createBaseModel();
+		BaseModel baseModelY = this.cloneBaseModel(baseModelX);
 
 		reflexivity(baseModelX);
+		symmetricity(baseModelX, baseModelY);
 		falseOnNull(baseModelX);
 	}
 
@@ -20,8 +22,19 @@ public class BaseModelTests {
 		return baseModel;
 	}
 
+	public BaseModel cloneBaseModel(BaseModel baseModel) {
+		BaseModel baseModelClone = new BaseModel(baseModel);
+		baseModelClone.setUpdatedAt(baseModel.getUpdatedAt());
+		return baseModelClone;
+	}
+
 	public void reflexivity(BaseModel baseModel) {
 		assertTrue(baseModel.equals(baseModel));
+	}
+
+	public void symmetricity(BaseModel baseModelX, BaseModel baseModelY) {
+		assertTrue(baseModelX.equals(baseModelY));
+		assertTrue(baseModelY.equals(baseModelX));
 	}
 
 	public void falseOnNull(BaseModel baseModel) {
