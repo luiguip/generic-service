68e0a3a8cc45c29d25f43061f7fbef9e3eaf5d88
luiguip <luisguilhermepdea@gmail.com>
2020-07-28 19:11:53 -0300

    [CHANGE] Tests on BaseModelImpl instead of BaseModel

---
+++ src/test/java/tech/swapy/abstract_service/base_model/BaseModelImplTests.java
@@ -0,0 +1,86 @@
+package tech.swapy.abstract_service.base_model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.time.LocalDateTime;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseModelImplTests {
+
+	BaseModelImpl baseModelImplX;
+	BaseModelImpl baseModelImplY;
+
+	@BeforeEach
+	void init() {
+		this.baseModelImplX = this.createBaseModelImpl();
+		this.baseModelImplY = this.cloneBaseModelImpl(baseModelImplX);
+	}
+
+	@Test
+	void shouldEqualsBeFalseWithNull() {
+		assertFalse(baseModelImplX.equals(null));
+	}
+	
+	@Test
+	void shouldEqualsBeFalseWithAnotherObject() {
+		assertFalse(baseModelImplX.equals(new String()));
+	}
+
+	@Test
+	void shouldEqualsBeFalseWithNullId() {
+		baseModelImplX.setId(null);
+		assertFalse(baseModelImplX.equals(baseModelImplX));
+	}
+
+	@Test
+	void shouldEqualsBeFalseWithDifferentIds() {
+		baseModelImplY.setId(baseModelImplX.getId() + 1);
+		assertFalse(baseModelImplY.equals(baseModelImplX));
+	}
+
+	@Test
+	void shouldEqualsBeFalseWithDifferentCreatedAt() {
+		baseModelImplY.setCreatedAt(LocalDateTime.now().plusSeconds(1));
+		baseModelImplY.setUpdatedAt(baseModelImplX.getUpdatedAt());
+		assertFalse(baseModelImplY.equals(baseModelImplX));
+	}
+
+	@Test
+	void shouldEqualsBeFalseWithDifferentUpdatedAt() {
+		baseModelImplY.setUpdatedAt(LocalDateTime.now().plusSeconds(1));
+		assertFalse(baseModelImplY.equals(baseModelImplX));
+	}
+
+	@Test
+	void shouldEqualsBeTrueWithSameObject() {
+		assertTrue(baseModelImplX.equals(baseModelImplX));
+	}
+
+	@Test
+	void shouldBeTrueWithAnotherObjectWithSameFields() {
+		assertTrue(baseModelImplX.equals(baseModelImplY));
+	}
+
+	@Test
+	void shouldHashCodeBeEqualsWithDifferentObjectsAndSameFields() {
+		assertEquals(baseModelImplX.hashCode(), baseModelImplY.hashCode());
+	}
+
+	private BaseModelImpl createBaseModelImpl() {
+		BaseModelImpl baseModelImpl = new BaseModelImpl();
+		baseModelImpl.setId(1L);
+		return baseModelImpl;
+	}
+
+	private BaseModelImpl cloneBaseModelImpl(BaseModelImpl baseModelImpl) {
+		BaseModelImpl baseModelImplClone = new BaseModelImpl(baseModelImpl);
+		baseModelImplClone.setUpdatedAt(baseModelImpl.getUpdatedAt());
+		return baseModelImplClone;
+	}
+}
