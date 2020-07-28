18b3c5269cca1835ef6950d5959cc306dd941165
luiguip <luisguilhermepdea@gmail.com>
2020-07-28 10:57:59 -0300

    [REFACTOR] eclipse refactor code

--- src/main/java/tech/swapy/abstract_service/base/BaseServiceImpl.java
+++ src/main/java/tech/swapy/abstract_service/base/BaseServiceImpl.java
@@ -11,55 +11,55 @@ import org.springframework.stereotype.Service;
 
 @Service
 @Transactional
-public abstract class BaseServiceImpl<T extends BaseModel, ID extends Serializable>
-implements BaseService<T, ID> {
-	   
+public abstract class BaseServiceImpl<T extends BaseModel, ID extends Serializable> implements BaseService<T, ID> {
+
 	@Autowired
-    private BaseRepository<T, ID> baseRepository;
-    
-	public BaseServiceImpl() {}
-    
-    public BaseServiceImpl(BaseRepository<T, ID> baseRepository) {
-        this.baseRepository = baseRepository;
-    }
-    
-    @Override
-    public T save(T entity) {
-        return (T) baseRepository.save(entity);
-    }
+	private BaseRepository<T, ID> baseRepository;
 
-    @Override
-    public List<T> findAll() {
-        return baseRepository.findAll();
-    }
+	public BaseServiceImpl() {
+	}
 
-    @Override
-    public Optional<T> findById(ID entityId) {
-        return baseRepository.findById(entityId);
-    }
+	public BaseServiceImpl(BaseRepository<T, ID> baseRepository) {
+		this.baseRepository = baseRepository;
+	}
 
-    @Override
-    public T update(T entity) {
-        return (T) baseRepository.save(entity);
-    }
+	@Override
+	public T save(T entity) {
+		return (T) baseRepository.save(entity);
+	}
 
-    @Override
-    public T updateById(T entity, ID entityId) {
-        Optional<T> optional = baseRepository.findById(entityId);
-        if(optional.isPresent()){
-            return (T) baseRepository.save(entity);
-        }else{
-            return null;
-        }
-    }
+	@Override
+	public List<T> findAll() {
+		return baseRepository.findAll();
+	}
 
-    @Override
-    public void delete(T entity) {
-        baseRepository.delete(entity);
-    }
+	@Override
+	public Optional<T> findById(ID entityId) {
+		return baseRepository.findById(entityId);
+	}
 
-    @Override
-    public void deleteById(ID entityId) {
-        baseRepository.deleteById(entityId);
-    }
+	@Override
+	public T update(T entity) {
+		return (T) baseRepository.save(entity);
+	}
+
+	@Override
+	public T updateById(T entity, ID entityId) {
+		Optional<T> optional = baseRepository.findById(entityId);
+		if (optional.isPresent()) {
+			return (T) baseRepository.save(entity);
+		} else {
+			return null;
+		}
+	}
+
+	@Override
+	public void delete(T entity) {
+		baseRepository.delete(entity);
+	}
+
+	@Override
+	public void deleteById(ID entityId) {
+		baseRepository.deleteById(entityId);
+	}
 }
