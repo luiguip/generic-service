da5b12d7f362eb10a394aeea485fb07bc2a6093f
luiguip <luisguilhermepdea@gmail.com>
2020-08-04 12:28:29 -0300

    IdNotFoundException added

---
+++ src/main/java/tech/swapy/abstract_service/base/domain/exceptions/IdNotFoundException.java
@@ -0,0 +1,11 @@
+package tech.swapy.abstract_service.base.domain.exceptions;
+
+public class IdNotFoundException extends Exception{
+
+	private static final long serialVersionUID = 667886627930305081L;
+
+	public IdNotFoundException(String message) {
+		super(message);
+	}
+	
+}
