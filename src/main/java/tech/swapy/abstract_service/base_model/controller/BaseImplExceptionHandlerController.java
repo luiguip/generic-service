6a2eff45367b83577304272928e7ef10fc2cd945
luiguip <luisguilhermepdea@gmail.com>
2020-08-04 18:02:20 -0300

    Exception handler

---
+++ src/main/java/tech/swapy/abstract_service/base_model/controller/BaseImplExceptionHandlerController.java
@@ -0,0 +1,14 @@
+package tech.swapy.abstract_service.base_model.controller;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import tech.swapy.abstract_service.base.controller.BaseExceptionHandlerController;
+
+@RestControllerAdvice
+public class BaseImplExceptionHandlerController extends BaseExceptionHandlerController {
+
+	public BaseImplExceptionHandlerController() {
+		super();
+	}
+
+}
