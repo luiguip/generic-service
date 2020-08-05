77b0ff8bfc57bb11ed5a401db05a93eb884ca97e
luiguip <luisguilhermepdea@gmail.com>
2020-08-05 15:43:01 -0300

    generic-domain module added

---
+++ generic-domain/src/main/java/tech/swapy/abstract_service/base/domain/BaseService.java
@@ -0,0 +1,18 @@
+package tech.swapy.abstract_service.base.domain;
+
+import java.io.Serializable;
+import java.util.List;
+
+public interface BaseService<E extends BaseDomainModel, ID extends Serializable> {
+
+	public abstract E save(E domainModel);
+
+	public abstract List<E> findAll();
+
+	public abstract E findById(ID entityId);
+
+	public abstract E updateById(ID entityId, E domainModel);
+
+	public abstract void deleteById(ID entityId);
+
+}
