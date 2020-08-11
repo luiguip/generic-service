c7c3931aa22e4cc953471f9491878f2d5a8e78d4
luiguip <luisguilhermepdea@gmail.com>
2020-08-11 10:14:05 -0300

    rest-client done~

---
+++ generic-client/src/main/java/tech/swapy/generic_service/client/BaseClient.java
@@ -0,0 +1,21 @@
+package tech.swapy.generic_service.client;
+
+import java.io.Serializable;
+import java.util.List;
+
+import tech.swapy.generic_service.controller.BaseComunicationModel;
+import tech.swapy.generic_service.domain.BaseDomainModel;
+
+public interface BaseClient<E extends BaseDomainModel, ID extends Serializable> {
+
+	public abstract E save(E domainModel);
+
+	public abstract List<E> findAll();
+
+	public abstract E findById(ID entityId);
+
+	public abstract E updateById(ID entityId, E domainModel);
+
+	public abstract void deleteById(ID entityId);
+
+}
---
+++ generic-client/src/main/java/tech/swapy/generic_service/client/rest/BaseRestClient.java
@@ -0,0 +1,77 @@
+package tech.swapy.generic_service.client.rest;
+
+import java.io.Serializable;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import tech.swapy.generic_service.client.BaseClient;
+import tech.swapy.generic_service.controller.BaseComunicationModel;
+import tech.swapy.generic_service.controller.BaseComunicationModelConverter;
+import tech.swapy.generic_service.domain.BaseDomainModel;
+
+public class BaseRestClient<T extends BaseComunicationModel, E extends BaseDomainModel, ID extends Serializable> implements BaseClient<E, ID> {
+
+	private Class<T> clazz;
+
+	private WebClient webClient;
+
+	@Autowired
+	private BaseComunicationModelConverter<T, E> baseComunicationModelConverter;
+
+	public BaseRestClient(String url, Class<T> clazz) {
+		this.webClient = WebClient.builder().baseUrl(url).build();
+		this.clazz = clazz;
+	}
+
+	@Override
+	public E save(E domainModel) {
+		T response = webClient.post()
+				.bodyValue(domainModel)
+				.retrieve()
+				.bodyToMono(clazz)
+				.block();
+		return baseComunicationModelConverter.convert(response);
+	}
+
+	@Override
+	public List<E> findAll() {
+		List<T> responseList = webClient.get()
+				.retrieve()
+				.bodyToMono(new ParameterizedTypeReference<List<T>>(){})
+				.block();
+		return baseComunicationModelConverter.convertComunicationModelList(responseList);
+	}
+
+	@Override
+	public E findById(ID entityId) {
+		T responseBody = webClient.get()
+				.uri("{id}", entityId)
+				.retrieve()
+				.bodyToMono(clazz)
+				.block();
+		return baseComunicationModelConverter.convert(responseBody);
+	}
+
+	@Override
+	public E updateById(ID entityId, E domainModel) {
+		T responseBody = webClient.post()
+				.uri("{id}", entityId)
+				.bodyValue(domainModel)
+				.retrieve()
+				.bodyToMono(clazz)
+				.block();
+		return baseComunicationModelConverter.convert(responseBody);
+	}
+
+	@Override
+	public void deleteById(ID entityId) {
+		webClient.post()
+			.uri("{id}", entityId)
+			.retrieve()
+			.toBodilessEntity()
+			.block();
+	}
+}
